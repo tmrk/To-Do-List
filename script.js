54e2b2adfddf4cfe93fe98b3e81b0365d6bc28da
tmrk <tamas.marki@gmail.com>
2022-05-11 16:29:42 +0200

    Added new methods

--- script.js
+++ script.js
@@ -2,38 +2,44 @@
 
 const D = document;
 const B = document.body;
+
 let listCount = 0;
 
 class ToDoList {
 
-    constructor (name, task) {
+    constructor (name, task, dateCreated) {
         listCount++;
         this.name = name || "To-Do-List" + numberOfLists;
         this.tasks = [];
-        this.viewConstruct();
+        this.dateCreated = dateCreated;
+        this.constructView();
         if (task) this.add(task);
     }
 
-    add (content, dateAdded = new Date(), dateDue) {
+    // ----- MODEL -----
+
+    add (content, dateCreated = new Date(), dateDue) {
         if (content) {
-            let newTask = new Task(content, dateAdded, dateDue);
+            let newTask = new Task(content, dateCreated, dateDue, this);
             this.tasks.push(newTask);
-            this.viewAdd(newTask);
+            this.addView(newTask);
             console.log("Added new task: " + content);
         } else console.error("No task specified");
     }
 
     remove (taskIndex = -1) {
-        if (taskIndex >= 0 && taskIndex <= this.tasks.length - 1) {
+        if (taskIndex == -1) console.error("No task specified");
+        else if (taskIndex >= 0 && taskIndex <= this.tasks.length - 1) {
+            this.removeView(this.tasks[taskIndex]);
             this.tasks.splice(taskIndex, 1);
             console.log("Task removed from index " + taskIndex);
-        } else console.error("No task specified or no task at the specified index");  
+        } else console.error("No task at the specified index " + taskIndex);
     }
 
     // Returns an array of tasks that include the searchterm
     search (searchTerm) {
         if (searchTerm) {
-            return this.tasks.filter(function (task) { 
+            return this.tasks.filter(function (task) {
               return task.content.includes(searchTerm);
             })
         } else console.error("No search term specified");
@@ -41,36 +47,79 @@ class ToDoList {
 
     // ----- VIEW -----
 
-    viewConstruct () {
+    constructView () {
         this.el = D.createElement("ul");
         this.el.id = this.name;
         B.appendChild(this.el);
     }
 
-    viewAdd (task) {
-        let newTask = D.createElement("li");
-        newTask.textContent = task.content;
-        newTask.dataset.dateAdded = task.dateAdded.getTime(); // milliseconds
-        if (task.dateDue) newTask.dataset.dateDue = task.dateDue.getTime(); // milliseconds
-        this.el.appendChild(newTask);
+    addView (task) {
+        this.el.appendChild(task.el);
+    }
+
+    removeView (task) {
+        this.el.removeChild(task.el);
     }
 
 }
 
 class Task {
-    
-    constructor (content, dateAdded, dateDue) {
+
+    constructor (content, dateCreated, dateDue, parent) {
         this.content = content;
-        this.dateAdded = dateAdded;
+        this.dateCreated = dateCreated;
         this.dateDue = dateDue;
+        this.parent = parent;
+
+        this.constructView ();
+    }
+
+    remove () {
+        let thisTask = this;
+        let thisIndex = this.parent.tasks.findIndex(task => {
+            return task === thisTask;
+        });
+        this.parent.remove(thisIndex);
+    }
+
+    constructView () {
+        this.el = D.createElement("li");
+        this.el.textContent = this.content;
+        this.el.dataset.dateCreated = this.dateCreated.getTime(); // milliseconds
+        if (this.dateDue) this.el.dataset.dateDue = this.dateDue.getTime(); // milliseconds
+
+        let buttonRemove = D.createElement("span");
+        buttonRemove.classList.add("remove");
+        buttonRemove.textContent = "X";
+        buttonRemove.addEventListener("click", () => this.remove());
+
+        this.el.appendChild(buttonRemove);
     }
 
 }
 
+// Create a new To Do List
 let toDoList = new ToDoList("myList");
 
+// Add some test data
 toDoList.add("hej");
 toDoList.add("foo");
 toDoList.add("bar");
 toDoList.add("bar2");
 
+
+// ----- CONTROLLER -----
+
+let formAddNewTask = D.createElement("form");
+let inputNewTaskContent = D.createElement("input");
+let buttonAddNewTask = D.createElement("button");
+buttonAddNewTask.type = "button"; // so form doesn't get submitted
+buttonAddNewTask.textContent = "Add Task";
+buttonAddNewTask.addEventListener("click", () => {
+  toDoList.add(inputNewTaskContent.value);
+  inputNewTaskContent.value = "";
+});
+formAddNewTask.appendChild(inputNewTaskContent);
+formAddNewTask.appendChild(buttonAddNewTask);
+
+B.appendChild(formAddNewTask);
